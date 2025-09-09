30f88e230f7de478f0957fea56af3de62237f51e
RileyRRivera <rileylachish.rivera@smc.pshs.edu.ph>
2025-09-09 10:47:10 +0800

    SG6-Code1.py

---
+++ SG6-Code1.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter your first number:"))
+num2 = float(input("Enter your second number:"))
+num3 = float(input("Enter your third number:"))
+sum = num1 + num2 + num3
+roundsum = round(sum, 2)
+print("The total of the three numbers is:", roundsum)
